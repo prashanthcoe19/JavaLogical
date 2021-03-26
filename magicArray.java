d1656329cdc0810d7caff05b05013e98942e86cf
Prashant <prashant.niroula67@gmail.com>
2021-03-26 13:39:58 +0545

    magicArray

---
+++ magicArray.java
@@ -0,0 +1,37 @@
+public class magicArray {
+    public static void main(String[] args) {
+        int[] arr = { 13, 5, -5, 5, 3 };
+        magic(arr);
+    }
+
+    public static int magic(int[] a) {
+        int sum = 0;
+        for (int i = 1; i < a.length; i++) {
+            if (isPrime(a[i]) == 1) {
+                sum = sum + a[i];
+            }
+        }
+        if (a[0] == sum || (a[0] == 0 & sum == 0)) {
+            System.out.println("Magic Array");
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int isPrime(int n) {
+        int flag = 0;
+        if (n < 0)
+            return 0;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+}
