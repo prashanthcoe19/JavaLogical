ebb69431b7083ff2f068da7f94013c7ab4b91992
Prashant <prashant.niroula67@gmail.com>
2021-03-31 00:31:14 +0545

    smallfactors

---
+++ smallFactors.java
@@ -0,0 +1,22 @@
+public class smallFactors {
+    public static void main(String[] args) {
+        int k = 12;
+        int n = 24;
+        sFactors(k, n);
+    }
+
+    public static boolean sFactors(int k, int n) {
+        int flag = 0;
+        for (int i = 2; i < k; i++) {
+            if ((n % i) == 0 & (n / i) < k) {
+                flag++;
+            }
+        }
+        if (flag > 0) {
+            System.out.println("true");
+            return true;
+        }
+        System.out.println("false");
+        return false;
+    }
+}
